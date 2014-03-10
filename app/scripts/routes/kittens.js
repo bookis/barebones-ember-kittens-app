17b9530d3e8683b836c43fb8f22c4926fac7b370
Bookis Smuin <bookis.smuin@gmail.com>
2014-03-10 16:23:04 -0700

    adds kittens

---
+++ app/scripts/routes/kittens.js
@@ -0,0 +1,5 @@
+App.KittensRoute = Ember.Route.extend({
+  model: function () {
+    return [1,2,3,3];
+  }
+})
--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -14,9 +14,23 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
 Ember.TEMPLATES["kittens"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var stack1, self=this;
+
+function program1(depth0,data) {
   
+  var buffer = '', stack1;
+  data.buffer.push("\n  ");
+  stack1 = helpers._triageMustache.call(depth0, "", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n  <img src=\"http://placekitten.com/");
+  stack1 = helpers._triageMustache.call(depth0, "", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("00/500\" />\n");
+  return buffer;
+  }
 
-
-  data.buffer.push("<img src=\"http://placekitten.com/500/500\" />");
+  stack1 = helpers.each.call(depth0, {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[],types:[],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  else { data.buffer.push(''); }
   
 });
