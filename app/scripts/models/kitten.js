ca28db2d73bec150e56c4dc74a50b48c5c7cb27d
Bookis Smuin <bookis.smuin@gmail.com>
2014-03-11 10:24:58 -0700

    adds computed properties

--- app/scripts/models/kitten.js
+++ app/scripts/models/kitten.js
@@ -1,17 +1,23 @@
 App.Kitten = DS.Model.extend({
   name: DS.attr("string"),
+  lastName: DS.attr("string"),
+  fullName: function () {
+    return this.get("name") + " " + this.get("lastName")
+  }.property("name", "lastName"),
   toys: DS.hasMany("toy", {async: true})
 })
 
 App.Kitten.FIXTURES = [
   {
     id: 1,
-    name: "Urchin Jr.",
+    name: "Urchin",
+    lastName: "Jr.",
     toys: [1,3]
   },
   {
     id: 2,
-    name: "Siberian Husky",
+    name: "Siberian",
+    lastName: "Husky",
     toys: [2]
   }
 ]
--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -26,7 +26,7 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
 Ember.TEMPLATES["kittens"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
-  var buffer = '', stack1, escapeExpression=this.escapeExpression, self=this;
+  var buffer = '', stack1, escapeExpression=this.escapeExpression, self=this, helperMissing=helpers.helperMissing;
 
 function program1(depth0,data) {
   
@@ -41,9 +41,9 @@ function program1(depth0,data) {
   }
 function program2(depth0,data) {
   
-  var buffer = '', stack1;
+  var buffer = '', stack1, helper, options;
   data.buffer.push("\n    <h2>");
-  stack1 = helpers._triageMustache.call(depth0, "name", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  stack1 = helpers._triageMustache.call(depth0, "fullName", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
   data.buffer.push("</h2>\n      <img src=\"http://placekitten.com/100/100\" \n        ");
   data.buffer.push(escapeExpression(helpers.action.call(depth0, "hoverKitten", "", {hash:{
@@ -54,7 +54,12 @@ function program2(depth0,data) {
   data.buffer.push(">Like</button>\n    <h4>Favs</h4>\n    <ul>\n      ");
   stack1 = helpers.each.call(depth0, "toys", {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(3, program3, data),contexts:[depth0],types:["ID"],data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-  data.buffer.push("\n    </ul>\n  ");
+  data.buffer.push("\n    </ul>\n    ");
+  data.buffer.push(escapeExpression((helper = helpers.input || (depth0 && depth0.input),options={hash:{
+    'type': ("text"),
+    'value': ("name")
+  },hashTypes:{'type': "STRING",'value': "ID"},hashContexts:{'type': depth0,'value': depth0},contexts:[],types:[],data:data},helper ? helper.call(depth0, options) : helperMissing.call(depth0, "input", options))));
+  data.buffer.push("\n  ");
   return buffer;
   }
 function program3(depth0,data) {
